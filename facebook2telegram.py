b91f0372ac5dd0d99ca58a74f414bc89b7cdcdce
pxdl <17756301+pxdl@users.noreply.github.com>
2017-06-03 01:08:05 -0300

    Implemented JSON save/load, autostart

--- facebook2telegram.py
+++ facebook2telegram.py
@@ -2,10 +2,12 @@
 from __future__ import unicode_literals         #Needed for youtube-dl
 import ast                                      #Used for ini settings
 import ConfigParser
+import json
 import logging
 from time import sleep
 import sys
 from datetime import datetime                   #Used for date comparison
+import urllib
 
 import telegram                                 #telegram-bot-python
 from telegram.ext import Updater
@@ -20,6 +22,7 @@ import youtube_dl
 
 #Logging
 logging.basicConfig(
+    filename='facebook2telegram.log',
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -27,38 +30,74 @@ logger = logging.getLogger(__name__)
 #youtube-dl
 ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
 
-#Read config file
-Config = ConfigParser.SafeConfigParser()
-Config.read('botsettings.ini')
+
+def loadSettingsFile(filename):
+    #Read config file
+    Config = ConfigParser.SafeConfigParser()
+    Config.read(filename)
+    #Load config
+    try:
+        global facebook_token
+        global facebook_pages
+        global facebook_refresh_rate
+        global allow_status
+        global allow_photo
+        global allow_video
+        global allow_link
+        global allow_shared
+        global allow_message
+        global telegram_token
+        global channel_id
+        global admin_id
+        facebook_token = Config.get('facebook', 'token')
+        facebook_pages = ast.literal_eval(Config.get("facebook", "pages"))
+        facebook_refresh_rate = Config.get('facebook', 'refreshrate')
+        allow_status = Config.getboolean('facebook', 'status')
+        allow_photo = Config.getboolean('facebook', 'photo')
+        allow_video = Config.getboolean('facebook', 'video')
+        allow_link = Config.getboolean('facebook', 'link')
+        allow_shared = Config.getboolean('facebook', 'shared')
+        allow_message = Config.getboolean('facebook', 'message')
+        telegram_token = Config.get('telegram', 'token')
+        channel_id = Config.get('telegram', 'channel')
+        admin_id = Config.get('telegram', 'admin')
+        print('Loaded settings:')
+        print('Channel: ' + channel_id)
+        print('Refresh rate: ' + facebook_refresh_rate)
+        print('Allow Status: ' + str(allow_status))
+        print('Allow Photo: ' + str(allow_photo))
+        print('Allow Video: ' + str(allow_video))
+        print('Allow Link: ' + str(allow_link))
+        print('Allow Shared: ' + str(allow_shared))
+        print('Allow Message: ' + str(allow_message))
+        return True
+    except ConfigParser.NoSectionError:
+        sys.exit('Fatal Error: Missing or invalid settings file.')
+    except ConfigParser.NoOptionError:
+        sys.exit('Fatal Error: Missing or invalid option in settings file.')
+    except ValueError:
+        sys.exit('Fatal Error: Missing or invalid value in settings file.')
+    except SyntaxError:
+        sys.exit('Fatal Error: Syntax error in page list.')
 
 
+def loadFacebookGraph(facebook_token):
+    global graph
+    graph = facebook.GraphAPI(access_token=facebook_token, version='2.7')
 
-#Load settings file
-try:
-    facebook_token = Config.get('facebook', 'token')
-    facebook_pages = ast.literal_eval(Config.get("facebook", "pages"))
-    facebook_refresh_rate = Config.get('facebook', 'refreshrate')
-    allow_status = Config.getboolean('facebook', 'status')
-    allow_photo = Config.getboolean('facebook', 'photo')
-    allow_video = Config.getboolean('facebook', 'video')
-    allow_link = Config.getboolean('facebook', 'link')
-    allow_shared = Config.getboolean('facebook', 'shared')
-    allow_message = Config.getboolean('facebook', 'message')
-    telegram_token = Config.get('telegram', 'token')
-    channel_id = Config.get('telegram', 'channel')
-except ConfigParser.NoSectionError:
-    sys.exit('Fatal Error: Missing or invalid settings file.')
-except ConfigParser.NoOptionError:
-    sys.exit('Fatal Error: Missing or invalid option in settings file.')
-except ValueError:
-    sys.exit('Fatal Error: Missing or invalid value in settings file.')
-except SyntaxError:
-    sys.exit('Fatal Error: Syntax error in page list.')
 
-#Facebook
-graph = facebook.GraphAPI(access_token=facebook_token, version='2.7')
-#page_count = 0
-#last_date_tg = 0
+def loadTelegramBot(telegram_token):
+    global bot
+    global updater
+    global dispatcher
+    global job_queue
+    try:
+        bot = telegram.Bot(token=telegram_token)
+    except InvalidToken:
+       sys.exit('Fatal Error: Invalid Telegram Token')
+    updater = Updater(token=telegram_token)
+    dispatcher = updater.dispatcher
+    job_queue = updater.job_queue
 
 
 def parsePostDate(post):
@@ -67,39 +106,73 @@ def parsePostDate(post):
     return post_date
 
 
-print('Getting most recent posts dates...')
-last_post_date = {}
-last_posts = graph.get_objects(
-            ids=facebook_pages,
-            fields='name,posts.limit(1){created_time}')
-for page in facebook_pages:
+class dateTimeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, datetime):
+            serial = o.isoformat()
+            return serial
+        raise TypeError('Unknown type')
+        return json.JSONEncoder.default(self, o)
+
+
+def dateTimeDecoder(pairs, format="%Y-%m-%dT%H:%M:%S"):
+    d = {}
+    for k, v in pairs:
+        if isinstance(v, basestring):
+            try:
+                d[k] = datetime.strptime(v, format)
+            except ValueError:
+                d[k] = v
+        else:
+            d[k] = v
+    return d
+
+
+def getMostRecentPostsDates():
+    print('Getting most recent posts dates...')
+    global last_posts_dates
+    last_posts_dates = {}
+    last_posts = graph.get_objects(
+                ids=facebook_pages,
+                fields='name,posts.limit(1){created_time}')
+    print('Trying to load JSON file...')
     try:
-        last_post_date[page] = parsePostDate(last_posts[page]['posts']['data'][0])
-        print('Page: '+last_posts[page]['name'])
-        print('Last updated: '+last_post_date[page].strftime('%Y-%m-%d %H:%M:%S +0')+'\n')
+        loadDatesJSON()
+        for page in facebook_pages:
+            if page not in last_posts_dates:
+                print('Checking if page '+page+' went online...')
+                try:
+                    last_posts_dates[page] = parsePostDate(last_posts[page]['posts']['data'][0])
+                    print('Page: '+last_posts[page]['name']+' went online.')
+                    dumpDatesJSON()
+                except KeyError:
+                    print('Page '+page+' not found.')
+
+    except (IOError, ValueError):
+        print('JSON file not found or corrupted, fetching latest dates...')
+        for page in facebook_pages:
+            try:
+                last_posts_dates[page] = parsePostDate(last_posts[page]['posts']['data'][0])
+                print('Checked page: '+last_posts[page]['name'])
+                #print('Last updated: '+last_posts_dates[page].strftime('%Y-%m-%d %H:%M:%S +0')+'\n')
+                
+            except KeyError:
+                print('Page '+page+' not found.')
+        dumpDatesJSON()
         
-    except KeyError:
-        print('Page '+page+' not found.')
 
-#print (last_post_date)
-print('Loaded settings:')
-print('Channel: ' + channel_id)
-print('Refresh rate: ' + facebook_refresh_rate)
-print('Allow Status: ' + str(allow_status))
-print('Allow Photo: ' + str(allow_photo))
-print('Allow Video: ' + str(allow_video))
-print('Allow Link: ' + str(allow_link))
-print('Allow Shared: ' + str(allow_shared))
-print('Allow Message: ' + str(allow_message))
+def loadDatesJSON():
+    global last_posts_dates
+    with open('dates.json', 'r') as f:
+        last_posts_dates = json.load(f, object_pairs_hook=dateTimeDecoder)
+    print('Loaded JSON file.')
 
-#Telegram
-try:
-    bot = telegram.Bot(token=telegram_token)
-except InvalidToken:
-   sys.exit('Fatal Error: Invalid Telegram Token')
-updater = Updater(token=telegram_token)
-dispatcher = updater.dispatcher
-job_queue = updater.job_queue
+
+def dumpDatesJSON():
+    global last_posts_dates
+    with open('dates.json', 'w') as f:
+        json.dump(last_posts_dates, f, sort_keys=True, indent=4, cls=dateTimeEncoder)
+    print('Dumped JSON file.')
 
 
 def getDirectURLVideo(video_id):
@@ -171,21 +244,39 @@ def checkIfAllowedAndPost(post, bot, chat_id):
 
 
 def postPhotoToChat(post, post_message, bot, chat_id):
-    bot.send_photo(
-        chat_id=chat_id,
-        photo=post['full_picture'],
-        caption=post_message)
+    direct_link = post['full_picture']
+    try:
+        bot.send_photo(
+            chat_id=chat_id,
+            photo=post['full_picture'],
+            caption=post_message)
+    except BadRequest:
+        try:    
+            print('Sending by URL failed, downloading file...')
+            urllib.urlretrieve(direct_link, 'temp.jpg')
+            print('Sending file...')
+            print(post_message)
+            picture = open('temp.jpg', 'rb')
+            bot.send_photo(
+                chat_id=chat_id,
+                photo=picture,  #Throws an unicode error, needs fix
+                caption=post_message)
+        except:
+            print('Could not send photo file, sending link...')
+            bot.send_message(    #Send direct link as a message
+                chat_id=chat_id,
+                text=direct_link+'\n'+post_message)
 
 
 def postVideoToChat(post, post_message, bot, chat_id):
-    #This function tries to pass 3 different URLs to the Telegram API
-    #instead of downloading the video file locally to save bandwidth.
-    #First link: Direct video source
-    #Second link: Direct video source gotten from youtube-dl
-    #Third link: Direct video source with smaller resolution
-    #If all three fail, it then (TODO: 4th OPTION - DOWNLOAD FILE LOCALLY
-    #FOR UPLOAD) sends the first link as a message, followed by the post's
-    #message
+    """This function tries to pass 3 different URLs to the Telegram API
+    instead of downloading the video file locally to save bandwidth.
+    First link: Direct video source
+    Second link: Direct video source gotten from youtube-dl
+    Third link: Direct video source with smaller resolution
+    If all three fail, it then (TODO: 4th OPTION - DOWNLOAD FILE LOCALLY
+    FOR UPLOAD) sends the first link as a message, followed by the post's
+    message"""
     #If youtube link, post the link
     if 'caption' in post and post['caption'] == 'youtube.com':
         print('Sending YouTube link...')
@@ -250,8 +341,18 @@ def periodicCheck(bot, job):
             fields='name,posts{\
                                full_picture,created_time,type,\
                                message,source,link,caption,parent_id,object_id}')
+        
+        if admin_id:
+            try:
+                bot.send_message(
+                    chat_id=admin_id,
+                    text='Successfully fetched Facebook posts.')
+            except TelegramError:
+                print('Admin ID not found.')
+                print('Successfully fetched Facebook posts.')
     except facebook.GraphAPIError:
         print('Error: Could not get Facebook posts.')
+        raise
         return
     for page in facebook_pages:
         try:
@@ -260,62 +361,67 @@ def periodicCheck(bot, job):
             
             #Get list of last 25 posts
             posts_data = pages_dict[page]['posts']['data']
-            new_posts = filter(lambda post: parsePostDate(post) > last_post_date[page], posts_data)
+            new_posts = filter(lambda post: parsePostDate(post) > last_posts_dates[page], posts_data)
             if not new_posts:
-                print('No new posts.')
-                continue
+                print('No new posts for this page.')
+                continue    #Goes to next iteration (page)
             for post in reversed(new_posts):   #Chronological order
                 try:
                     print('Posting NEW post...')
                     postToChat(post, bot, chat_id)
-                    last_post_date[page] = parsePostDate(post)
+                    last_posts_dates[page] = parsePostDate(post)
                 except BadRequest:
-                    print('Error: Telegram chat not found')
+                    print('Error: Telegram could not send the message')
+                    raise
                     return
+            dumpDatesJSON()
 
         except KeyError:
             print('Page not found.')
             continue
+    print('Checked all pages. Next check in '+facebook_refresh_rate+' seconds.')
 
 
-def createSubscription(bot, update, job_queue, chat_data):
-    job_checkNew = Job(
-        periodicCheck, float(facebook_refresh_rate),
-        repeat=True, context=channel_id)
+def createSubscription(bot):
+    job_checkNew = Job(periodicCheck, float(facebook_refresh_rate),
+                       repeat=True, context=channel_id)
     job_queue.put(job_checkNew)
-    chat_data['job'] = job_checkNew
-
-    update.message.reply_text('Subscribed.')
-
-
-def deleteSubscription(bot, update, chat_data):
-    if 'job' not in chat_data:
-        update.message.reply_text('You are not subscribed.')
-        return
-
-    job_checkNew = chat_data['job']
-    job_checkNew.schedule_removal()
-    del chat_data['job']
-
-    update.message.reply_text('Unsubscribed.')
+    print('Job created.')
+    if admin_id:
+        try:
+            bot.send_message(
+                chat_id=admin_id,
+                text='Bot Started.')
+        except TelegramError:
+            print('Admin ID not found.')
+            print('Bot Started.')
 
 
 def error(bot, update, error):
     logger.warn('Update "{}" caused error "{}"'.format(update, error))
-    
 
-# on different commands - answer in Telegram
-subscription_handler = CommandHandler('subscribe', createSubscription,
-                                      pass_job_queue=True,
-                                      pass_chat_data=True)
-unsubscribe_handler = CommandHandler('unsubscribe', deleteSubscription,
-                                     pass_chat_data=True)
-# on command message
-dispatcher.add_handler(subscription_handler)
-dispatcher.add_handler(unsubscribe_handler)
-# log all errors
-dispatcher.add_error_handler(error)
 
-updater.start_polling()
+def main():
+    loadSettingsFile('botsettings.ini')
+    loadFacebookGraph(facebook_token)
+    loadTelegramBot(telegram_token)
+    getMostRecentPostsDates()
+    createSubscription(bot)
+    # on different commands - answer in Telegram
+    #subscription_handler = CommandHandler('subscribe', createSubscription,
+    #                                      pass_job_queue=True,
+    #                                      pass_chat_data=True)
+    #unsubscribe_handler = CommandHandler('unsubscribe', deleteSubscription,
+    #                                     pass_chat_data=True)
+    # on command message
+    #dispatcher.add_handler(subscription_handler)
+    #dispatcher.add_handler(unsubscribe_handler)
+    # log all errors
+    dispatcher.add_error_handler(error)
 
-updater.idle()
+    updater.start_polling()
+
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
